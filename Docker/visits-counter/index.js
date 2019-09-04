f87868363079fec36983f9059caab7f045a4762d
kman4you <khubaibahmad1994@gmail.com>
2019-09-05 01:41:58 +0500

    add redis-server in redis client host

--- Docker/visits-counter/index.js
+++ Docker/visits-counter/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const redis = require("redis");
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  host: "redis-server"
+});
 client.set("visits", 0);
 app.get("/", (req, res) => {
   client.get("visits", (err, visits) => {
